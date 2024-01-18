54465cc3b786241236f16cf5504ebb0f8d0964ef
Isabell <isabell.leoson@gmail.com>
2024-01-18 17:33:20 +0100

    head structure

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -8,12 +8,11 @@ interface HeadProps {
 // component to handle dynamic head data
 const Head: React.FC<HeadProps> = ({ titel, metaDescription }) => {
   return (
-    <>
-      <html lang="en" />
-      {metaDescription && <meta name="description" content={metaDescription} />}
+    <html lang="en">
+      <meta name="description" content={metaDescription} />
       <title>{titel}</title>
       <link rel="canonical" href="https://ileosonportfolio.netlify.app/" />
-    </>
+    </html>
   );
 };
 
