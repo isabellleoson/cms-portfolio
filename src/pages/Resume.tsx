5bcf461a555d9a48cb4ebe05bc36eed46eb4dd51
Isabell <isabell.leoson@gmail.com>
2024-01-18 19:15:56 +0100

    added slug to url

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -148,11 +148,14 @@ export const Head: React.FC<PageProps<PortfolioQuery>> = ({ data }) => {
         <>
           <title>{node.title}</title>
           <meta name="description" content={node.metaDescription} />
+          <link
+            rel="canonical"
+            href={`https://ileosonportfolio.netlify.app/Resume${node.slug}`}
+          />
         </>
       ))}
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="canonical" href="https://ileosonportfolio.netlify.app/" />
     </>
   );
 };
